4ad47556c40ba0af45fdea35075c999d96c160fa
Chu-son <s15mh133@mail.saitama-u.ac.jp>
2015-09-15 03:31:31 +0900

    Updated

--- ArduinoSerialGUI/ArduinoSerialGUI.cs
+++ ArduinoSerialGUI/ArduinoSerialGUI.cs
@@ -14,7 +14,6 @@ namespace ArduinoSerialGUI
 {
     public partial class ArduinoSerialGUI : Form
     {
-
         int[] baudrate = { 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200 };
 
         //データ格納用インナークラス
